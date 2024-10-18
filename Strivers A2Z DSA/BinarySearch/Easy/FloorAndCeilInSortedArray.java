a2c4317d594f01d5b9e5f61618f123a3fce4a465
AnkitShah9 <ankitshah123440@gmail.com>
2024-10-18 13:11:49 +0530

    Added new easy binary search problem

---
+++ Strivers A2Z DSA/BinarySearch/Easy/FloorAndCeilInSortedArray.java
@@ -0,0 +1,36 @@
+/*
+    Problem Link: https://takeuforward.org/arrays/floor-and-ceil-in-sorted-array/
+    Solution Link: https://youtu.be/6zhGS79oQ4k
+*/
+
+public class FloorAndCeilInSortedArray {
+
+    public static void main(String[] args) {
+        // Smallest index such as arr[index] > num
+        int[] arr = {3, 4, 4, 7, 8, 10}; // {3, 4, 4, 7, 8, 10}
+        int x = 5;
+        int size = arr.length;
+        int floor=-1, ceiling=-1;
+        int low=0, high=size-1, mid;
+
+        while (low <= high) {
+            mid = (low+high)/2;
+
+            int midVal = arr[mid];
+            if(midVal == x) {
+                floor = ceiling = midVal;
+                break;
+            } else if(midVal < x) {
+                floor = midVal;
+                low = mid+1;
+            } else {
+                ceiling = midVal;
+                high = mid-1;
+            }
+        }
+
+        System.out.printf("Floor value: %d, Ceiling bound: %d", floor, ceiling);
+    }
+
+
+}
