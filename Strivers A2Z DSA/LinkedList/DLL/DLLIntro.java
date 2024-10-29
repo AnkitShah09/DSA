3b7a33a4599442a78e1a94e06830987ab9c18ee9
AnkitShah9 <ankitshah123440@gmail.com>
2024-10-29 23:42:11 +0530

    Added basic utilities for doubly linked list

--- Strivers A2Z DSA/LinkedList/DLL/DLLIntro.java
+++ Strivers A2Z DSA/LinkedList/DLL/DLLIntro.java
@@ -13,7 +13,85 @@ public class DLLIntro {
         head = DllNodeUtils.convertArrayToLinkedList(arr, head);
         DllNodeUtils.printLinkedList(head);
 
-        head = DllNodeUtils.addNodeInLinkedList(9, head);
+//        head = DllNodeUtils.addNodeInLinkedList(9, head);
+//        DllNodeUtils.printLinkedList(head);
+//
+//        head = deleteHeadOfDLL(head);
+//        DllNodeUtils.printLinkedList(head);
+//
+//        head = deleteTailOfDLL(head);
+//        DllNodeUtils.printLinkedList(head);
+//
+//        int k = 3;
+//        head = deleteKthElementOfDLL(head, k);
+//        DllNodeUtils.printLinkedList(head);
+
+        head = deleteNode(head.nextNode);
         DllNodeUtils.printLinkedList(head);
+    }
+
+    private static DllNode deleteNode(DllNode node) {
+        return null;
+    }
+
+    private static DllNode deleteKthElementOfDLL(DllNode head, int k) {
+
+        DllNode temp = head;
+        int counter = 0;
+
+        while (temp != null) {
+            counter++;
+            if(counter == k) {
+                break;
+            }
+            temp = temp.nextNode;
+        }
+
+        if(temp == null) {
+            return null;
+        }
+
+        if(temp.prevNode == null && temp.nextNode == null) {
+            return null;
+        } else if (temp.prevNode == null) {
+            return deleteHeadOfDLL(head);
+        } else if (temp.nextNode == null) {
+            return deleteTailOfDLL(head);
+        } else {
+            DllNode prev = temp.prevNode;
+            DllNode next = temp.nextNode;
+
+            prev.nextNode = next;
+            next.prevNode = prev;
+
+            temp.prevNode = null;
+            temp.nextNode = null;
+            return head;
+        }
+    }
+
+    private static DllNode deleteTailOfDLL(DllNode head) {
+        if(head == null || head.nextNode == null) {
+            return null;
+        }
+        DllNode temp = head;
+        while (temp.nextNode != null) {
+            temp = temp.nextNode;
+        }
+        temp = temp.prevNode;
+        temp.nextNode.prevNode = null;
+        temp.nextNode = null;
+        return head;
+    }
+
+    private static DllNode deleteHeadOfDLL(DllNode head) {
+
+        if(head == null || head.nextNode == null) {
+            return null;
+        }
+        DllNode temp = head.nextNode;
+        head.nextNode = null;
+        head = temp;
+        return head;
     }
 }
